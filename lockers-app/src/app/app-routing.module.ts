6303357491bc449687455132c80e87d5f768cac3
Pierre Christmann <talzatior@gmail.com>
2021-10-11 21:43:22 +0200

    Added menu component, not-found component + routing base

--- lockers-app/src/app/app-routing.module.ts
+++ lockers-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: MenuComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lockers-app/src/app/app.module.ts
+++ lockers-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { GenderComponent } from './shared/components/gender/gender.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    NotFoundComponent,
+    GenderComponent
   ],
   imports: [
     BrowserModule,
